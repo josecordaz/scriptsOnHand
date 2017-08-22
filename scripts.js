7a569a3d4dcacac625490e697761a2378cf6d4b7
Jose Carlos Ordaz <josecordaz@gmail.com>
2017-08-21 22:11:17 -0500

    init new script window

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-var clipboard = new Clipboard('.btn');
-clipboard.on('success', function(e) {
-    e.clearSelection();
-});
+window.onload = () =>{
+    var clipboard = new Clipboard('.bodyId');
+    clipboard.on('success', function(e) {
+        e.clearSelection();
+    });
+};
+
+window.openNewScript = function(){
+    let newScript = document.createElement('div');
+    newScript.className = 'main-pop-up';
+    newScript.innerHTML = 'PANTALLA EMERGENTE';
+    document.querySelector('body').appendChild(newScript);
+}
